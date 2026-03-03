0fa645542de6cc85d59ed04b80a260087e37040b
Kiyum <kiyum1406@gmail.com>
2026-03-03 13:17:39 -0800

    added dotenv file

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // Import the express module
 import express from 'express';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+// load environment variables from .env
+dotenv.config();
+// console.log(process.env.DB_HOST);
 
 // Create an express appliocation
 const app = express();
@@ -11,6 +17,9 @@ const HOST = "0.0.0.0";
 // Enable static file serving 
 app.use(express.static('public'));
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 //"Middleware" that allows express to read
 // form data and store it in req.body 
 app.use(express.urlencoded({extended: true}));
@@ -18,26 +27,56 @@ app.use(express.urlencoded({extended: true}));
 //create a temp array to store orders
 const orders = [];
 
+// Create a pool (bucket) of database connections
+const pool = mysql2.createPool({
+    // host: '143.198.224.58',
+    // user: 'admin',
+    // password: 'adminpass0603',
+    // database: 'pizza_db',
+    // port: 3306
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
+
+// Database test route 
+app.get('/db-test', async(req, res) => {
+    try {
+        const pizza_orders = await pool.query('SELECT * FROM orders');
+        res.send(pizza_orders[0]);
+    } catch(err) {
+        console.error('Database error: ', err);
+    }
+});
+
+
 // Define our main route ('/')
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    //res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 });
 
 
 // Define our contact-us route ('/')
 app.get('/contact-us', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+    //res.sendFile(`${import.meta.dirname}/views/contact.html`);
+    res.render('contact');
 });
 
 
 // Define our /thank-you route ('/')
 app.get('/thank-you', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    res.render('confirmation');
 });
 
 // admin route ('/')
 app.get('/admin', (req, res) => {
-    res.send(orders);
+    //res.send(orders);
+    res.render('admin', { orders });
 });
 
 // Define /sumbit-order route ('/')
@@ -60,10 +99,13 @@ app.post('/submit-order', (req, res) => {
 
     //res.send(orders);
     // res.send(req.body);
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+
+    //res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+    res.render('confirmation', { order });
 });
 
 // Start server and listed on designed port
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running at http://${HOST}:${PORT}`);
+    //console.log(`Server is running at http://${HOST}:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
